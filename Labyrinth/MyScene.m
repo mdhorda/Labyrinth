7d80c299f2db716446a43694262f085dae6297f4
Mehul Dhorda <run2thesun@gmail.com>
2014-09-06 13:42:43 -0700

    - Use physics to move ball around and create collisions - Use touch events to move ball for now since I left iPad at work and can’t test accelerometer on simulator - Added a test wall and finishing hole

--- Labyrinth/MyScene.m
+++ Labyrinth/MyScene.m
@@ -7,37 +7,73 @@
 //
 
 #import "MyScene.h"
+#import <math.h>
 
-#define BALL_SIZE 50
+#define BALL_DIAM 50
+#define BALL_RADIUS BALL_DIAM / 2
+
+#define BACKGROUND_COLOR [SKColor colorWithRed:0.85 green:0.85 blue:0.66 alpha:1.0]
+#define BLACK_COLOR [SKColor colorWithRed:0 green:0 blue:0 alpha:1.0]
 
 @interface MyScene()
 {
     SKSpriteNode *ball;
+    SKSpriteNode *hole;
 }
 @end
 
 @implementation MyScene
 
--(id)initWithSize:(CGSize)size {    
+-(id)initWithSize:(CGSize)size
+{
     if (self = [super initWithSize:size]) {
-        self.backgroundColor = [SKColor colorWithRed:0.85 green:0.85 blue:0.66 alpha:1.0];
+        self.backgroundColor = BACKGROUND_COLOR;
         
+        // Set physics body so that ball doesn't go outside the screen bounds
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+        
+        // Create wall
+        SKSpriteNode *wall = [SKSpriteNode spriteNodeWithColor:BLACK_COLOR size:CGSizeMake(600, 10)];
+        wall.position = CGPointMake(0, 200);
+        wall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:wall.size];
+        wall.physicsBody.dynamic = NO;
+        wall.physicsBody.resting = YES;
+        [self addChild:wall];
+        
+        // Create hole
+        hole = [SKSpriteNode spriteNodeWithImageNamed:@"BlackHole"];
+        hole.size = CGSizeMake(BALL_DIAM, BALL_DIAM);
+        hole.position = CGPointMake(self.size.width - BALL_RADIUS, BALL_RADIUS);
+        [self addChild:hole];
+        
+        // Create ball
         ball = [SKSpriteNode spriteNodeWithImageNamed:@"GreenBall"];
-        ball.size = CGSizeMake(BALL_SIZE, BALL_SIZE);
-        ball.position = CGPointMake(BALL_SIZE / 2, self.size.height - (BALL_SIZE / 2));
-        SKAction *action = [SKAction moveByX:BALL_SIZE y:0 duration:0.5];
-        [ball runAction:[SKAction repeatActionForever:action]];
+        ball.size = CGSizeMake(BALL_DIAM, BALL_DIAM);
+        ball.position = CGPointMake(BALL_RADIUS, self.size.height - BALL_RADIUS);
+        ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:BALL_RADIUS];
+        ball.physicsBody.dynamic = YES;
         [self addChild:ball];
     }
     return self;
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
-    if (ball.position.x >= (self.size.width - (BALL_SIZE / 2)))
-    {
-        [ball removeAllActions];
-    }
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // Use touch event to apply impulse to ball and make it move
+    UITouch *touch = [touches anyObject];
+    CGPoint newLocation = [touch locationInView:self.view];
+    CGPoint prevLocation = [touch previousLocationInView:self.view];
+    float xOffset = newLocation.x - prevLocation.x;
+    
+    [ball.physicsBody applyImpulse:CGVectorMake(xOffset / 10, 0)];
+}
+
+-(void)update:(CFTimeInterval)currentTime
+{
+    // Check if ball is in the hole
+    if (fabsf(ball.position.x - hole.position.x) < 3 &&
+        fabsf(ball.position.y - hole.position.y) < 3)
+        ball.position = CGPointMake(BALL_RADIUS, self.size.height - BALL_RADIUS);
 }
 
 @end
